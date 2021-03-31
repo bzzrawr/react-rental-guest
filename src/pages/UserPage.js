61741460a5d5369cb21a837b97b153a7b032e8a8
syafiq ali <49745060+bzzrawr@users.noreply.github.com>
2021-04-01 00:57:56 +0800

    sign-in page

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import {useParams}  from 'react-router-dom'
 
 
 export default function UserPage() {
-  let {id} = useParams()
     return (
       <div>
-        <h1> Hey there user {id}</h1>
+        <h1> Hey there user</h1>
         <p>Home user profile page</p>
       </div>
     )

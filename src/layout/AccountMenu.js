fe8c77596474b54a8d65039fa6ffcd4f44c82457
syafiq ali <49745060+bzzrawr@users.noreply.github.com>
2021-04-01 23:48:33 +0800

    modified user account part

---
+++ src/layout/AccountMenu.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import TopNav from './TopNav'
+import Menu from './Menu'
+import AccountSettings from './AccountSettings'
+
+
+export default function BookingMenu() {
+    return (
+        <div>
+            <TopNav/>
+            <Menu/>
+            <AccountSettings/>
+        </div>
+        
+
+    )
+}
+
--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import AccountMenu from '../layout/BookingMenu'
-import {Link} from 'react-router-dom'
-// import BookingMenu from '../layout/BookingMenu'
+import AccountMenu from '../layout/AccountMenu'
 
 export default function Account() {
     return (
-      <div className="login-layout">
-        <h2>hehehe</h2>
-        <Link to="/Bookings">click</Link>
+      <div className="overall-layout">
+        <AccountMenu/>
       </div>
     )
 }
